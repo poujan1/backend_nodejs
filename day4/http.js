245ed932a8bd14bb42c87b77a9db857c9d91f217
Pujan <ppoujan@gmail.com>
2025-09-07 12:16:27 +0545

    created http server

--- day4/http.js
+++ day4/http.js
@@ -1,3 +1,28 @@
 // http module
+// aws
+// url , routes
+// http server
+// === ==
+// express
+// MERN - Mongoose Expresss React Node
 
 const http = require("http");
+// req, res object
+const server = http.createServer((req, res) => {
+  if (req.url === "/profile") {
+    const name = "swastika";
+    res.end(`Hello ${name} your profile fetched successfully`);
+  } else {
+    res.end("hell0");
+  }
+});
+// port
+
+const port = 1123;
+server.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`server is running on port ${port}`);
+  }
+});
